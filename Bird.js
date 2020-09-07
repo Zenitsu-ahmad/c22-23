02b42cba191b910a1d21e330c7f6adce644d1147
Zenitsu-ahmad <70284354+Zenitsu-ahmad@users.noreply.github.com>
2020-09-07 15:36:56 -0400

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,28 @@
+class Bird {
+    constructor (x,y)  {
+        var options = {
+            restitution : 0.5,
+            friction : 1.0,
+            density : 1.5
+        }  
+      this.body = Bodies.rectangle(x,y,70,70,options);
+      this.width = 70;
+      this.height = 70;
+      World.add(world,this.body);
+    }
+    
+    display () {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+    
+      rectMode(CENTER);
+      strokeWeight(3);
+      stroke("blue");
+      fill ("red");
+      rect(0,0,this.width,this.height);
+      pop();
+    }
+    }
